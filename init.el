0d7e022cc75787fee4c55eb0a493f3bc09ef1328
JRobinson28 <joseph28robinson@gmail.com>
2022-10-19 14:53:43 +0100

    Add more specific config

--- init.el
+++ init.el
@@ -25,15 +25,21 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Navigation package (ido, helm or ivy?)
-
-;;(ido-mode t)
+;; Navigation
 
 (use-package swiper
   :bind (("C-s" . swiper)))
 
 (use-package ivy
   :config (ivy-mode 1))
+
+(use-package projectile
+  :ensure t
+  :init (projectile-mode +1)
+  :custom ((setq projectile-project-search-path '("~/projects/")))
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
 
 ;; Doom modeline
 
@@ -43,4 +49,48 @@
   :custom ((doom-modeline-height 15)))
 
 
-;; TODO: paredit, cider, clojure, Terraform, projectile, magit
+;; Magit
+
+(use-package magit
+  :ensure t)
+
+;; Clojure
+
+(use-package paredit
+  :ensure t
+  :init (enable-paredit-mode))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init (rainbow-delimiters-mode))
+
+(use-package clojure-mode
+  :ensure t
+  :config
+  (add-hook 'clojure-mode-hook #'paredit-mode)
+  (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode))
+
+(use-package cider
+  :ensure t
+  :config
+  (setq nrepl-log-messages t)
+  (add-hook 'cider-repl-mode-hook #'paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode))
+
+;; Terraform
+
+(use-package terraform-mode
+  :ensure t
+  :config (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode))
+
+(use-package terraform-doc
+  :ensure t)
+
+;; Markdown
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown"))
+
+
