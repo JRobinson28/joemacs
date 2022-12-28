460518473ec6dcbedbd7cc68b94710a4a6a6d665
JRobinson28 <joseph28robinson@gmail.com>
2022-12-28 15:21:25 +0000

    Add diff-hl

--- config/joemacs-git.el
+++ config/joemacs-git.el
@@ -2,6 +2,13 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+(use-package diff-hl
+  :init (global-diff-hl-mode +1)
+  :hook (magit-post-refresh-hook . diff-hl-magit-post-refresh))
+
+;(use-package git-gutter
+;  :init (global-git-gutter-mode +1))
+
 (use-package forge
   :custom
   (setq  forge-topic-list-limit '(100 . 0)))
