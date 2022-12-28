77c5b187a39b4b11f50da2c84208c1abfdc83ad2
JRobinson28 <joseph28robinson@gmail.com>
2022-12-28 15:31:19 +0000

    Add magit-file-dispatch binding

--- config/joemacs-git.el
+++ config/joemacs-git.el
@@ -1,6 +1,7 @@
 (use-package magit
   :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
+  :bind ("C-c g" . magit-file-dispatch))
 
 (use-package diff-hl
   :init (global-diff-hl-mode +1)
